ecf8bf3123883ebaf685c7b524d073571de692c5
Josh Martin <joshualmartin@gmail.com>
2017-03-08 16:36:57 -0800

    updated

--- createGolem.py
+++ createGolem.py
@@ -1,6 +1,9 @@
 from mcpi.minecraft import Minecraft
-
 mc = Minecraft.create()
+
+mc.postToChat("Let's make some Golems!")
+
+
 
 #This will get the player's position
 position = mc.player.getTilePos()
@@ -9,23 +12,24 @@ x = position.x
 y = position.y
 z = position.z
 
-#Creates the Golem based on the position
-blockType = 42
-mc.setBlock(x, y, z, blockType)
-y = y + 1
-mc.setBlock(x, y, z, blockType)
-x = x + 1
-mc.setBlock(x, y, z, blockType)
-x = x + -2
-mc.setBlock(x, y, z, blockType)
-x = x + 1
-y = y + 1
-blockType = 91
-mc.setBlock(x, y, z, blockType)
+count = 0
+if count < 10:
+    count += 1
+    
+    blockType = 42
 
-#This will attempt to set the player to the original position.
-x = position.x
-y = position.y
-z = position.z
+    mc.setBlock(x, y, z, blockType)
+    y = y + 1
 
-mc.player.setTilePos(x, y, z)
+    mc.setBlock(x, y, z, blockType)
+    x = x + 1
+
+    mc.setBlock(x, y, z, blockType)
+    x = x + -2
+
+    mc.setBlock(x, y, z, blockType)
+    x = x + 1
+    y = y + 1
+
+    blockType = 91
+    mc.setBlock(x, y, z, blockType)
